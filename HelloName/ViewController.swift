3f8932d4417c489e0425b53835f25a1b49a81080
ryanburns31892 <rburns@chapman.edu>
2016-11-09 19:30:18 -0800

    first commit

--- HelloName/ViewController.swift
+++ HelloName/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var helloLabel: UILabel!
+	@IBOutlet weak var nameTextField: UITextField!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		print("View did load")
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	override func viewDidAppear(_ animated: Bool) {
+		
+		print("View did appear")
 	}
-
-
+	
+	@IBAction func onNameChanged(_ sender: AnyObject) {
+		
+		updateLabel()
+	}
+	@IBAction func onTapGo(_ sender: AnyObject) {
+		
+		print("Button Tapped!")
+		
+		updateLabel()
+	}
+	
+	func updateLabel()
+	{
+		if let name = self.nameTextField.text
+		{
+			self.helloLabel.text = "Hello \(name)"
+		}
+	}
 }
 
